b172e3a6e7f16eb1be8e1ed841699f5ee3b8a952
TMFrancis <BostonTomF@gmail.com>
2021-12-02 22:07:40 -0500

    Map with a couple test points & popups  - baby steps ;)  - will start work on Python script to    build geoJson file

---
+++ data/fatalPoints.js
@@ -0,0 +1,8 @@
+var fatalPoints = {"type":"FeatureCollection","features":[
+{"type":"Feature", "id":"01","properties":{"date": "2009-09-11 00:00:00","name":"Tracy Milillo","age":"22","city":  "Brookline","link":  "http://archive.boston.com/yourtown/news/brookline/2009/10/ghost_bike_appears_at_fatality.html"},
+"geometry":{"type":"Point","coordinates":[-71.112426,42.334878]}},
+{"type":"Feature", "id":"02","properties":{"date": "2009-09-11 00:00:00","name":"Colin Fitzpatrick Alloyius D'Aguiar","age":"22","city":  "Brookline","link":  "http://archive.boston.com/yourtown/news/brookline/2009/10/ghost_bike_appears_at_fatality.html"},
+"geometry":{"type":"Point","coordinates":[-71.379535,42.282508]}},
+{"type":"Feature", "id":"03","properties":{"date": "2009-09-11 00:00:00","name":"Eric Michael Hunt","age":"22","city":  "Brookline","link":  "http://archive.boston.com/yourtown/news/brookline/2009/10/ghost_bike_appears_at_fatality.html"},
+"geometry":{"type":"Point","coordinates":[-71.112085,42.331995]}}
+]};
