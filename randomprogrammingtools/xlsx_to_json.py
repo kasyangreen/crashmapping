580bf30b2243155401291e7311fd1b36e5386f2a
TMFrancis <BostonTomF@gmail.com>
2021-12-06 19:08:07 -0500

    Generalized Python scripts - will ask user to choose xlsx file and write output to the directory xlsx file came from. Includes data format instructions

--- randomprogrammingtools/xlsx_to_json.py
+++ randomprogrammingtools/xlsx_to_json.py
@@ -8,13 +8,31 @@
 # Copyright:   (c) bosto 2021
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+# Note on data format. For best results, Excel spreadsheet should:
+#   - have 9 columns with the following headings:
+#       -date, name, age, city, mode, lat, lon & link
+#   - cells empty cells should have the value 'none'
+#   - City names should have the 1st letter capitalized
+#   - City names should have subsequent letters in lower case
+#   - City names should follow offical vocabulary (i.e. Attleboro &
+#       North Attleborough)
+#   - not use double quotes in cell entries
+#   - each cell should include only one data item (no multiple links)
+#   - output will be written to the Excel spreadsheet directory
+#   - output file name is fatalData.json
 
 import pandas as pd
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-# hard coded paths and file names - todo: allow user to choose file; set path file directory
-path = r"C:\Users\bosto\Documents\ClarkGIS\WebMapping\FinalProj"
-data = r"\FatalData_20162020_clean.xlsx"
+finder = tk.Tk()
+finder.withdraw()
+
+my_filetypes = (['Excel spreadsheets', '.xlsx'])
+dataXLSX = tk.filedialog.askopenfilename(title="Please choose new fata data spreadsheet",
+                                        filetypes=[("Excel files", ".xlsx .xls")])
+path=os.path.dirname(dataXLSX)
 
 data_type={'Date': str,
     'Name': str,
@@ -26,7 +44,7 @@ data_type={'Date': str,
     'Link': str
     }
 
-data_file = pd.read_excel(path + data, index_col=None, dtype=data_type, na_filter = False)
+data_file = pd.read_excel(dataXLSX, index_col=None, dtype=data_type, na_filter = False)
 
 try:
         file = open(path + '\FatalData.json',"w")
